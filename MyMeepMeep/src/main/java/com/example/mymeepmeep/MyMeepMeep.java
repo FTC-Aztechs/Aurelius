4589c3b82cb141b387b091081e9fec3c8b3899b6
scorpneo <scorpneo@gmail.com>
2023-11-26 16:46:53 -0600

    Wrote trajectories for all 3 poses on BlueLong, retuned Roadrunner

--- MyMeepMeep/src/main/java/com/example/mymeepmeep/MyMeepMeep.java
+++ MyMeepMeep/src/main/java/com/example/mymeepmeep/MyMeepMeep.java
@@ -3,6 +3,8 @@ package com.example.mymeepmeep;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -11,9 +13,11 @@ public class MyMeepMeep {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
         Pose2d StartPos = new Pose2d(-36,62,Math.toRadians(-90));
+        Pose2d rStartPos = new Pose2d(-36,-62,Math.toRadians(90));
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         //Blue Long Pos 3
@@ -32,10 +36,33 @@ public class MyMeepMeep {
                                 .strafeTo(new Vector2d(82 + StartPos.getX(), -50 + StartPos.getY()))
                                 .build()
                 );
+
+        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        //Blue Long Pos 3
+                        drive.trajectorySequenceBuilder(new Pose2d(0 + rStartPos.getX(), 0 + rStartPos.getY(), Math.toRadians(180) + rStartPos.getHeading()))
+                                //purple
+                                .setReversed(true)
+                                .splineToLinearHeading(new Pose2d(2 + rStartPos.getX(), 28 + rStartPos.getY(), Math.toRadians(-90) + rStartPos.getHeading()), Math.toRadians(90))
+                                //strafe to in front of gate
+                                .setReversed(false)
+                                .strafeTo(new Vector2d(2 + rStartPos.getX(), 50  + rStartPos.getY()))
+                                //strafe through gate
+                                .lineToLinearHeading(new Pose2d(50 + rStartPos.getX(), 50  + rStartPos.getY(), Math.toRadians(-90) + rStartPos.getHeading()))
+                                //spline to board and turn around
+                                .splineToLinearHeading(new Pose2d(82 + rStartPos.getX(), 22 + rStartPos.getY(), Math.toRadians(90) + rStartPos.getHeading()), Math.toRadians(-90))
+                                //park
+                                .strafeTo(new Vector2d(82 + rStartPos.getX(), 50 + rStartPos.getY()))
+                                .build()
+                );
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(myFirstBot)
+                .addEntity(mySecondBot)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Meet3/Aura_AutoRed_Short_Meet3.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Meet3;
+
+public class Aura_AutoRed_Short_Meet3 {
+}
